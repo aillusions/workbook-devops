f2fcf7a32a46d08fe766fa0e0b38f391ebffe74c
alex <alex@platin.io>
2019-06-23 16:32:52 +0300

    Terraform with ansible - getting started

---
+++ terraform/example.tf
@@ -0,0 +1,16 @@
+
+# https://www.terraform.io/docs/providers/aws/r/instance.html
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "oz_devops_user"
+  version = "~> 2.14"
+}
+
+resource "aws_instance" "ansible_instance" {
+  ami           = "ami-0ff760d16d9497662"
+  instance_type = "t2.nano"
+  key_name = "zalizniak_com_aws"
+  security_groups = ["oz_ec2_ansible_instance_sg"]
+  iam_instance_profile = "oz_ec2_ansible_instance_role"
+}
---
+++ terraform/terraform.md
@@ -0,0 +1,25 @@
+
+
+Create oz_devops_user
+    + AmazonEC2FullAccess
+    + AmazonSSMFullAccess
+
+Install https://learn.hashicorp.com/terraform/getting-started/install.html
+
+Add to PATH
+
+cd terraform
+
+terraform init
+
+terraform version
+
+    Terraform v0.12.1
+    + provider.aws v2.14.0
+
+terraform plan
+
+terraform apply
+
+terraform destroy
+
