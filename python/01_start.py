d782d63a6dc725806141fcc204c0a9a9eb2a7ac9
alex <alex@platin.io>
2019-06-29 11:19:46 +0300

    Python - wip

--- python/01_start.py
+++ python/01_start.py
@@ -1,3 +1,33 @@
-
+# comment
 
 print("Hello, World!")
+
+# amount = float(input('amount ?'))
+amount = 5
+
+result = amount - 1
+
+print("result: %0.4f ", result)
+
+condition = False
+
+if condition:
+    print("You can continue with the program ", type(condition))
+else:
+    print("The program will end here ", type(condition))
+
+num = 12345678901234567891312312312313123131231231231231231231083234234234123123123123123123L
+num = num * num
+
+print("num: ", num)
+
+flt = 12345678901234567891312312312313123131231231231231231231083234234234123123123123123123.123123131231231231231231
+flt = flt * flt
+
+print("flt: ", flt)
+
+strVal = """A multiline string
+starts and ends with
+a triple quotation mark."""
+
+print "strVal: ", strVal
